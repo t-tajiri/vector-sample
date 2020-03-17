dd4cc18b680a04cfee2a640196f75dabbfa92d67
t-tajiri <swallow.ava@gmail.com>
2020-03-17 19:26:13 +0900

    create constructor at sample vector

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+pub struct SampleVector<T> {
+    elements: Box<T>,
+    len: usize
+}
+
+impl<T: Default> SampleVector<T> {
+    pub fn new() -> Self {
+        Self::with_capacity(0)
+    }
+
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self {
+            elements: Self::allocate_in_heap(capacity),
+            len: 0
+        }
+    }
+
+    fn allocate_in_heap(size: usize) -> Box<[T]> {
+        std::iter::repeat_with(Default::default)
+            .take(size)
+            .collect::<Vec<_>>()
+            .into_boxed_slice()
+    }
+}
